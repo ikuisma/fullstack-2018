574badab496e9ee19433f29df1c727b4632dc95e
ikuisma <ilkka.a.kuisma@gmail.com>
2018-01-18 02:16:12 +0200

    Finished exercise 1.1

---
+++ week-1/first-app/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.title}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return props.sisalto.map((rivi, i) =>
+        <p key={i}>{rivi.osa} {rivi.tehtava}</p>
+    )
+}
+
+const Yhteensa = (props) => {
+    const yhteensa = props.sisalto.map(element => element.tehtava).reduce((prev, curr) => prev + curr)
+    return (
+        <p>yhteensä {yhteensa} tehtävää</p>
+    )
+}
+
+function rivi(osa, tehtava) {
+    return {'osa': osa, 'tehtava': tehtava}
+}
+
+const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const sisalto = [
+        rivi('Reactin perusteet', 10),
+        rivi('Tiedonvälitys propseilla', 7),
+        rivi('Komponenttien tila', 14)
+    ]
+    return (
+        <div>
+            <Otsikko title={kurssi}/>
+            <Sisalto sisalto={sisalto}/>
+            <Yhteensa sisalto={sisalto}/>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
