8fa33723b677b67195799f32d954d8f51a27f2ad
ikuisma <ilkka.a.kuisma@gmail.com>
2018-01-22 00:52:59 +0200

    Finished exercise 1.14

--- week-1/anekdootit/src/index.js
+++ week-1/anekdootit/src/index.js
@@ -10,6 +10,18 @@ const randomExcluding = (max, exclude) => {
     return number
 }
 
+const maxValueIndex = (values) => {
+    const reducer = (acc, value, index, values) => (values[acc] < value) ? index : acc
+    return values.reduce(reducer, 0)
+}
+
+const Anecdote = ({text, votes}) => (
+    <div>
+        <p>{text}</p>
+        <p>has {votes} votes</p>
+    </div>
+)
+
 class App extends React.Component {
     constructor(props) {
         super(props)
@@ -31,12 +43,15 @@ class App extends React.Component {
     }
 
     render() {
+        const mostVotedAnecdote = maxValueIndex(this.state.votes)
         return (
             <div>
-                <p>{this.props.anecdotes[this.state.selected]}</p>
-                <p>has {this.state.votes[this.state.selected]} votes</p>
+                <Anecdote text={this.props.anecdotes[this.state.selected]} votes={this.state.votes[this.state.selected]}/>
+                <p></p>
                 <button onClick={this.voteFor(this.state.selected)}>vote</button>
                 <button onClick={this.randomPickSelected}>next anecdote</button>
+                <h2>anecdote with most votes:</h2>
+                <Anecdote text={this.props.anecdotes[mostVotedAnecdote]} votes={this.state.votes[mostVotedAnecdote]}/>
             </div>
         )
     }
