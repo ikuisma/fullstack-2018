0a3faad1a8b0d71b4a6d345bff19a07384c5d52b
ikuisma <ilkka.a.kuisma@gmail.com>
2018-01-22 10:23:50 +0200

    Added a copy of a project directory from week 1 to week 2.

---
+++ week-2/first-app/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    )
+}
+
+const Osa = (props, key) => {
+    return (
+        <p key={key}>{props.osa} {props.tehtava}</p>
+    )
+}
+
+const Sisalto = (props) => {
+    return props.osat.map((rivi, i) =>
+        <Osa key={i} osa={rivi.osa} tehtava={rivi.tehtava}/>
+    )
+}
+
+const Yhteensa = (props) => {
+    const yhteensa = props.osat.map(element => element.tehtava).reduce((prev, curr) => prev + curr)
+    return (
+        <p>yhteensä {yhteensa} tehtävää</p>
+    )
+}
+
+const rivi = function(osa, tehtava) {
+    return {'osa': osa, 'tehtava': tehtava}
+}
+
+const App = () => {
+    const kurssi = {
+        nimi : 'Half Stack -sovelluskehitys',
+        osat : [
+            rivi('Reactin perusteet', 10),
+            rivi('Tiedonvälitys propseilla', 7),
+            rivi('Komponenttien tila', 14)
+        ]
+    }
+    return (
+        <div>
+            <Otsikko kurssi={kurssi.nimi}/>
+            <Sisalto osat={kurssi.osat}/>
+            <Yhteensa osat={kurssi.osat}/>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
