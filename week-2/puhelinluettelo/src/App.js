7bfc2129fbe547934fadfab5b1b564b7bf2e91f6
ikuisma <ilkka.a.kuisma@gmail.com>
2018-01-25 12:07:09 +0200

    Finished exercise 2.6

--- week-2/puhelinluettelo/src/App.js
+++ week-2/puhelinluettelo/src/App.js
@@ -11,20 +11,36 @@ class App extends Component {
         }
     }
 
+    handleNameChange = (event) => {
+        const newName = event.target.value
+        this.setState({newName})
+    }
+
+    addPerson = (event) => {
+        event.preventDefault()
+        const persons = this.state.persons.concat({'name': this.state.newName})
+        this.setState({
+            persons,
+            newName: ''
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Puhelinluettelo</h2>
-                <form>
+                <form onSubmit={this.addPerson}>
                     <div>
-                        nimi: <input />
+                        nimi: <input value={this.state.newName} onChange={this.handleNameChange}/>
                     </div>
                     <div>
                         <button type="submit">lisää</button>
                     </div>
                 </form>
                 <h2>Numerot</h2>
-                ...
+                <ul>
+                    {this.state.persons.map((person) => <li key={person.name}>{person.name}</li>)}
+                </ul>
             </div>
         )
     }
