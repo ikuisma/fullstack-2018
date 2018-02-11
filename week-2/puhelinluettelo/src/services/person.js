b95124474e2156f62cd2bf2b004251cb25ffeb9d
ikuisma <ilkka.a.kuisma@gmail.com>
2018-02-11 22:44:46 +0200

    Front end changes to accommodate week 3's exercise.

--- week-2/puhelinluettelo/src/services/person.js
+++ week-2/puhelinluettelo/src/services/person.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/persons'
+const serverUrl = '/'
+const baseUrl = `${serverUrl}api/persons`
 
 const resourceUrlFor = (person) => `${baseUrl}/${person.id}`
 
