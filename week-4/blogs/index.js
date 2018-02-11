73f2d3cf5f517af5f3592890dc091145a1d5a924
ikuisma <ilkka.a.kuisma@gmail.com>
2018-02-11 23:08:07 +0200

    Moved mlab database url to .env file.

--- week-4/blogs/index.js
+++ week-4/blogs/index.js
@@ -17,7 +17,12 @@ module.exports = Blog
 app.use(cors())
 app.use(bodyParser.json())
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+if ( process.env.NODE_ENV !== 'production' ) {
+    require('dotenv').config()
+}
+
+const mongoUrl = process.env.MONGODB_URI
+
 mongoose.connect(mongoUrl)
 mongoose.Promise = global.Promise
 
