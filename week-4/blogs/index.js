a0e3e58c45fd7e72591b56d70c71e25579ab87d0
ikuisma <ilkka.a.kuisma@gmail.com>
2018-02-11 23:39:34 +0200

    Moved routes to separate controller file.

--- week-4/blogs/index.js
+++ week-4/blogs/index.js
@@ -4,10 +4,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./model/blog')
+const notesRouter = require('./controllers/notes')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', notesRouter)
 
 if ( process.env.NODE_ENV !== 'production' ) {
     require('dotenv').config()
@@ -18,23 +19,6 @@ const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 mongoose.Promise = global.Promise
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
