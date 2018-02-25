7fcca8665b21a0076e860939b0b77d84e629e6af
ikuisma <ilkka.a.kuisma@gmail.com>
2018-02-25 22:15:28 +0200

    Moved all api tests to same file to get rid of annoying error.

--- week-4/blogs/tests/api.test.js
+++ week-4/blogs/tests/api.test.js
@@ -1,8 +1,9 @@
 const supertest = require('supertest')
 const { app, server } = require('../index')
 const Blog = require('../model/blog')
+const User = require('../model/user')
 const api = supertest(app)
-const { initialBlogs, blogsInDb } = require('./test_helper')
+const { initialBlogs, blogsInDb, usersInDb } = require('./test_helper')
 
 const initialiseDatabase = async () => {
     await Blog.remove({})
@@ -128,6 +129,43 @@ describe('update blogs', () => {
         expect(response.body.likes).toBe(newLikes)
     })
 
+})
+
+describe('when there is only one user in the database', () => {
+
+    beforeAll(async () => {
+        await User.remove({})
+        const user = new User({username: 'PaavoUser', name:'Paavo', password: 'Ovaap', adult: true})
+        await user.save()
+    })
+    
+    test('GET /api/users returns all users in the database', async () => {
+        const usersBefore = await usersInDb()
+        const response = await api.get('/api/users')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body.length).toBe(usersBefore.length)
+    })
+
+    test('POST /api/users succeeds with a new username', async () => {
+        const newUser = {
+            username: 'MasiUser',
+            name: 'Masi',
+            password: 'Isam',
+            adult: true
+        }
+        const usersBefore = await usersInDb()
+        await api.post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        const usersAfter = await usersInDb()
+        expect(usersAfter.length).toBe(usersBefore.length + 1)
+    })
+        
+    afterAll(async () => {
+        await server.close()
+    })
 })
 
 afterAll(async () => {
