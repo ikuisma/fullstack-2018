5acd238e4143a9cfaf9dcb8db54328dd2e0cfd54
ikuisma <ilkka.a.kuisma@gmail.com>
2018-02-26 21:58:59 +0200

    Finished exercise 5.1

--- week-5/bloglist-frontend/src/App.js
+++ week-5/bloglist-frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      user: null,
+      username: '',
+      password: ''
     }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   componentDidMount() {
@@ -16,10 +24,40 @@ class App extends React.Component {
     )
   } 
 
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+      this.setState({user, username: '', password: ''})
+    } catch (exception) {
+      console.log('Whoops!')
+    }
+  }
+
   render() {
+    if (this.state.user === null) {
+      return (
+        <div>
+          <h2>Kirjaudu sovellukseen</h2>
+          <form onSubmit={this.login}>
+            <div>
+              Käyttäjätunnus <input type="text" name="username" value={this.state.username} onChange={this.handleLoginFieldChange}/>
+            </div>
+            <div>
+              Salasana <input type="password" name="password" value={this.state.password} onChange={this.handleLoginFieldChange}/>
+            </div>
+            <button type="submit">Kirjaudu</button>
+          </form>
+        </div>
+      )
+    }
     return (
       <div>
         <h2>blogs</h2>
+        <p>{this.state.user.name} logged in. </p>
         {this.state.blogs.map(blog => 
           <Blog key={blog._id} blog={blog}/>
         )}
