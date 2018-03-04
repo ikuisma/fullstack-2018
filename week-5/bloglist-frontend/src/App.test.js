d7705f89402ce910024d4c8a02d9798c608aa550
ikuisma <ilkka.a.kuisma@gmail.com>
2018-03-04 20:22:41 +0200

    Finished exercise 5.16

--- week-5/bloglist-frontend/src/App.test.js
+++ week-5/bloglist-frontend/src/App.test.js
@@ -4,21 +4,46 @@ import App from './App'
 import LoginForm from './components/LoginForm'
 import Blog from './components/Blog'
 jest.mock('./services/blogs')
+import blogService from './services/blogs'
 
-describe.only('<App/>', () => {
+describe('<App/>', () => {
 
     let app
 
-    beforeAll(() => {
-        app = mount(<App/>)
+    describe('when user is not logged in', () => {
+
+        beforeEach(() => {
+            app = mount(<App/>)
+        })
+
+        it('shows login form and no blogs when user has not yet logged in. ', () => {
+            app.update()
+            const blogComponents = app.find(Blog)
+            const loginFormComponents = app.find(LoginForm)
+            expect(blogComponents.length).toBe(0)
+            expect(loginFormComponents.length).toBe(1)
+        })
+
     })
 
-    it('shows login form and no blogs when user has not yet logged in. ', () => {
-        app.update()
-        const blogComponents = app.find(Blog)
-        const loginFormComponents = app.find(LoginForm)
-        expect(blogComponents.length).toBe(0)
-        expect(loginFormComponents.length).toBe(1)
+    describe('when user is logged in', () => {
+
+        beforeAll(() => {
+            const user = {
+                username: 'tester',
+                token: '1231231214',
+                name: 'Teuvo Testaaja'
+            }
+            localStorage.setItem('user', JSON.stringify(user))
+            app = mount(<App/>)
+        })
+
+        it('page contains correct amount of Blog elements', () => {
+            app.update()
+            const blogElements = app.find(Blog)
+            expect(blogElements.length).toBe(blogService.blogs.length)
+        })
+
     })
 
 })
--- week-5/bloglist-frontend/src/services/__mocks__/blogs.js
+++ week-5/bloglist-frontend/src/services/__mocks__/blogs.js
@@ -1,9 +1,24 @@
 let token = null
 
-const blogs = []
+const blogs = [
+    {
+        _id: '1',
+        title: 'Blog title 1',
+        author: 'Author 1',
+        url: 'www.google.com'
+    },{
+        _id: '2',
+        title: 'Blog title 2',
+        author: 'Author 1',
+        url: 'www.google.com'
+    }
+]
 
 const getAll = () => {
     return Promise.resolve(blogs)
 }
 
-export default { getAll, blogs }
+const setToken = () => {
+}
+
+export default { getAll, setToken, blogs }
