fbcd4328421321a535a9880419f0aabd10f71290
ikuisma <ilkka.a.kuisma@gmail.com>
2018-03-05 20:02:58 +0200

    Finished exercise 6.1

---
+++ week-6/redux-anecdotes-v2/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest/globals": true
+    },
+    "parser": "babel-eslint",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "jest"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        "react/prop-types": 0
+    }
+};
--- week-6/redux-anecdotes-v2/src/components/AnecdoteList.js
+++ week-6/redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -1,29 +1,29 @@
 import React from 'react'
 
 class AnecdoteList extends React.Component {
-  render() {
-    const anecdotes = this.props.store.getState()
-    return (
-      <div>
-        <h2>Anecdotes</h2>
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-          <div key={anecdote.id}>
+    render() {
+        const anecdotes = this.props.store.getState()
+        return (
             <div>
-              {anecdote.content}
-            </div>
-            <div>
+                <h2>Anecdotes</h2>
+                {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+                    <div key={anecdote.id}>
+                        <div>
+                            {anecdote.content}
+                        </div>
+                        <div>
               has {anecdote.votes}
-              <button onClick={() => 
-                this.props.store.dispatch({ type: 'VOTE', id: anecdote.id })
-              }>
+                            <button onClick={() => 
+                                this.props.store.dispatch({ type: 'VOTE', id: anecdote.id })
+                            }>
                 vote
-              </button>
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default AnecdoteList
