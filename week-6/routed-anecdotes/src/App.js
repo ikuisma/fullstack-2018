d719d654d7c8abd5abcc40f6b432996efca3186e
ikuisma <ilkka.a.kuisma@gmail.com>
2018-03-11 14:02:37 +0200

    Finished exercise 6.22

--- week-6/routed-anecdotes/src/App.js
+++ week-6/routed-anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Link, NavLink, Route } from 'react-router-dom'
-import { Container, Menu, Table } from 'semantic-ui-react'
+import { Container, Menu, Table, Grid, Image } from 'semantic-ui-react'
 
 const MenuLink = ({route, title}) => {
   const activeStyle = {
@@ -42,17 +42,24 @@ const AnecdoteList = ({ anecdotes }) => (
 )
 
 const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
+  <Grid>
+    <Grid.Row>
+      <Grid.Column width={10}>
+          <h2>About anecdote app</h2>
+          <p>According to Wikipedia:</p>
+          
+          <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+            Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+            such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+            An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
+          <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <Image src='http://cs-exhibitions.uni-klu.ac.at/fileadmin/template/documents/picture/EWD_thinking_1963.jpg'/>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 )
 
 const Footer = () => (
